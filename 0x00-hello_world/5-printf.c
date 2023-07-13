f6589c7b60b93b39f6cb2c831e1ff181f1d6805c
Abdallah Sameh Ragab <93019811+Abdallah-Ragab@users.noreply.github.com>
2023-07-13 17:28:27 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc -c $CFILE
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: c program to print using printf
+*
+* Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
