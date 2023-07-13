10596985f6d92b0ab38922218fb5f3fa88a34214
Abdallah Sameh Ragab <93019811+Abdallah-Ragab@users.noreply.github.com>
2023-07-13 17:51:06 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,17 @@
-#!/bin/bash
-gcc -c $CFILE
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: c program to print the sizes of different data types
+*
+* Return: 0
+*/
+int main(void)
+{
+printf("size of a char: $d byte(s)\n", sizeof(char));
+printf("size of a int: $d byte(s)\n", sizeof(int));
+printf("size of a long int: $d byte(s)\n", sizeof(long int));
+printf("size of a long long int: $d byte(s)\n", sizeof(long long int));
+printf("size of a float: $d byte(s)\n", sizeof(float));
+return (0);
+}
