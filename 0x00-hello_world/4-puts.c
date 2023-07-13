747440217d72c57d07ad0e26339dd787b79ca811
Abdallah Sameh Ragab <93019811+Abdallah-Ragab@users.noreply.github.com>
2023-07-13 17:20:55 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-int main(void){
-  puts("\"Programming is like building a multilingual puzzle\n");
+/*
+* main - Entry point
+*
+* Description: c program to print using puts
+*
+*/
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
   return 0;
 }
