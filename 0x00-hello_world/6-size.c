91314dfc4a3354bcb65d27523bf2ec98cbea71b9
Abdallah Sameh Ragab <93019811+Abdallah-Ragab@users.noreply.github.com>
2023-07-13 17:58:09 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ printf("size of a char: %u byte(s)\n", sizeof(char));
 printf("size of an int: %u byte(s)\n", sizeof(int));
 printf("size of a long int: %u byte(s)\n", sizeof(long int));
 printf("size of a long long int: %u byte(s)\n", sizeof(long long int));
-printf("size of a float: %u byte(s)\n, sizeof(float));
+printf("size of a float: %u byte(s)\n", sizeof(float));
 return (0);
 }
