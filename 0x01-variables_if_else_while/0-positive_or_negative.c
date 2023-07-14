7438beed1cd9b8601e076672be13f308d87ae13a
Abdallah Ragab <abdallahsameh1140@gmail.com>
2023-07-15 02:50:28 +0300

    fuck betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,27 +5,31 @@
 /**
  * main - Entry point
  *
+ * Description: Generates a random number and determines if it is positive,
+ *              negative, or zero.
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-int n;
+    int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
 
-if (n > 0) 
-{
-printf("%d is positive\n", n);
-}
-else if (n < 0)
-{
-printf("%d is negative\n", n);
-}    
-else
-{
-printf("%d is zero\n", n);
-}
-return (0);
+    if (n > 0)
+    {
+        printf("%d is positive\n", n);
+    }
+    else if (n < 0)
+    {
+        printf("%d is negative\n", n);
+    }
+    else
+    {
+        printf("%d is zero\n", n);
+    }
+
+    return (0);
 }
