b3432083c3b97d627a3a438936a1a154c6628aa8
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-20 19:21:30 +0300

    strcat, stringcopy, stringlenght

---
+++ 0x16-simple_shell/stringlength.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/**
+ * _strlen - length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
