b39dddf3c854985418db40a5c5ba4dd51c8a6b5d
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-20 19:19:30 +0300

    shell.h

---
+++ 0x16-simple_shell/shell.h
@@ -0,0 +1,36 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <stdbool.h>
+#define MAX_C 10
+/**
+ * struct denum - structure that contains vars
+ * @cnt: lines cnt
+ */
+typedef struct denum
+{
+	int cnt;
+} denum;
+void prompt(char **arv, char **envp, bool flg);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *handle_path(char **rgv, char *cmd);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+void handle_exit(char *cmd);
+void runcmd(char **rgv, char **arv, char **envp);
+char *trim(char *cmd);
+char *_strncpy(char *dest, char *src, int n);
+char *get_path(char *cmd);
+char *_getenv(char *name);
+char **tokenize_env(char *path);
+extern char **environ;
+/*void geterror(int cnt, char **arv, char *cmd);*/
+void geterror(denum *n, char **arv, char *cmd);
+/*void search_path(char **rgv, char *cmd, char **envp);*/
+#endif
