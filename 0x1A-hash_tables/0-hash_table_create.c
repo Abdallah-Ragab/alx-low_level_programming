e8cb63cfb4ae621a1f844f1b98347cf95d971960
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-08 11:39:03 +0200

    fix style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,15 +13,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
-		return NULL;
+		return(NULL);
 	hash_table->size = size;
 	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
-		return NULL;
+		return(NULL);
 	}
 	for (i = 0; i < size; i++)
 		hash_table->array[i] = NULL;
-	return hash_table;
+	return(hash_table);
 }
